8c16ed96e6d558f5fff6da607274ed1266f10d07
sljohnson32 <sljohnson32@gmail.com>
2017-08-30 09:20:45 -0600

    Update app container to be more descriptive

--- app/index.js
+++ app/index.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 import{ devToolsEnhancer } from 'redux-devtools-extension'
-import CurrentMoviesContainer from './containers/CurrentMoviesContainer'
+import App from './containers/AppContainer'
 
 const store = createStore(rootReducer, devToolsEnhancer())
 
 ReactDOM.render(
   <Provider store={ store }>
-    <CurrentMoviesContainer />
+    <App />
   </Provider>,
   document.getElementById('main')
 )
